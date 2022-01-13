bd454f9bafe1ac159f003e54f48d2a66b7b199e8
Girish <girish.vasmatkar@teliolabs.com>
2022-01-13 11:59:09 +0530

    Payout config server initialization

---
+++ src/test/java/com/paymaya/payout/config/server/PayoutConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymaya.payout.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayoutConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
